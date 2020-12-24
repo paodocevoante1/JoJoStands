76a729dd31e8f55e0a5ed80dd454f3043f74a2fa
paodocevoante1 <76541124+paodocevoante1@users.noreply.github.com>
2020-12-24 14:52:15 -0400

    Update AwakenedAmulet.cs

--- Items/Accessories/AwakenedAmulet.cs
+++ Items/Accessories/AwakenedAmulet.cs
@@ -10,7 +10,7 @@ namespace JoJoStands.Items.Accessories
         public override void SetStaticDefaults()
         {
             Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(10, 8));
-            Tooltip.SetDefault("An amulet that perfectly represents and enchances the form of the soul.\n30% increased Stand attack damage\n2 increased Stand Speed\n20% Stand Ability cooldown reduction\n30% increased Stand crit chance\nMakes melee stands inflict Infected on enemies.\nIncreased defense while the Stand is out");
+            Tooltip.SetDefault("Um amuleto que representa e melhora perfeitamente a forma da alma. 30% de dano de ataque de stand aumentada, 2 Velocidade de stand aumentada, 20% Redução de resfriamento da Habilidade de stand aumentada, 30% de chance de crítica de stand aumentada, Faz ataques stands de corpo a corpo infligir debuff Infectados nos inimigos e aumento de defesa enquanto o Stand está fora");
         }
 
         public override void SetDefaults()
